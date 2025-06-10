e560e9018be1a85510870ede0082834df7e069fc
Enoch Graham <enochgraham003@outlook.com>
2025-06-10 11:01:15 +0300

    feat(expenses): add RolesGuard to ExpensesController for user role protection feat(reports): include User entity in TypeOrmModule for ReportsModule

--- src/expenses/expenses.controller.ts
+++ src/expenses/expenses.controller.ts
@@ -1,32 +1,41 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, UseGuards } from '@nestjs/common';
 import { ExpensesService } from './expenses.service';
 import { CreateExpenseDto } from './dto/create-expense.dto';
 import { UpdateExpenseDto } from './dto/update-expense.dto';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/users/dto/create-user.dto';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @Controller('expenses')
+@UseGuards(RolesGuard)
 export class ExpensesController {
   constructor(private readonly expensesService: ExpensesService) {}
 
+  @Roles(Role.USER)
   @Post()
   create(@Body() createExpenseDto: CreateExpenseDto) {
     return this.expensesService.create(createExpenseDto);
   }
 
+  @Roles(Role.USER)
   @Get()
   findAll() {
     return this.expensesService.findAll();
   }
 
+  @Roles(Role.USER)
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.expensesService.findOne(id);
   }
 
+  @Roles(Role.USER)
   @Patch(':id')
   update(@Param('id', ParseIntPipe) id: number, @Body() updateExpenseDto: UpdateExpenseDto) {
     return this.expensesService.update(id, updateExpenseDto);
   }
 
+  @Roles(Role.USER)
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.expensesService.remove(id);
--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -5,11 +5,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Report } from './entities/report.entity';
 import { DatabaseModule } from 'src/database/database.module';
 import { Expense } from 'src/expenses/entities/expense.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
   imports: [
     DatabaseModule,
-    TypeOrmModule.forFeature([ Report, Expense])
+    TypeOrmModule.forFeature([ Report, Expense, User])
   ],
   controllers: [ReportsController],
   providers: [ReportsService],
